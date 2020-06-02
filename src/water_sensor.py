54f7171b29e11b13bf056baad7b3ed05464f738e
Chester Chee <chester.chee@gmail.com>
2020-06-02 00:39:36 -0400

    Cleanup

--- src/water_sensor.py
+++ src/water_sensor.py
@@ -4,10 +4,24 @@
 # Contactless Water Level Sensor class - instance of this class monitor water level
 # XKC-Y25-V                              and then publishes to MQTT for value changes.
 # Brown  - 5V
-# Yellow - OUT (to any GPIO pin as INPUT)
+# Yellow - OUT (signal output, to any GPIO pin as INPUT)
 # Blue   - GND
-# Black  - Mode (toggle to adjust sensor sensitive)
+# Black  - Mode
 #
+# Mode
+# When black wire connects high level, yellow wire (output signal) is positive, and 
+#   if there is induction, it outputs high level
+#   if there is no induction, it outputs low level
+# When black wire connects low  level, yellow wire (output signal) is neagtive, and
+#   if there is induction, it outputs low level
+#   if there is no induction, it outputs high level
+#
+# Sensor sensitivity adjustment
+# 1. Open the back cover of the sensor head, use small screwdriver to adjust the
+#    sensitivity knob
+# 2. If the knob is rotated counter clockwise, it increases sensitivity,
+#    otherwise decreases its sensitivity.
+
 import RPi.GPIO as GPIO
 from sensor import INTERVAL
 from sensor import Sensor
@@ -22,13 +36,14 @@ class WaterLevelSensor(Sensor):
 
     def __init__(self, name, channel, mode, mqtthost, mqttport, topic, qos, retain, debug):
         super().__init__(name, channel, INTERVAL, None, mqtthost, mqttport, topic, qos, retain, debug)
+        GPIO.setup(mode, GPIO.OUT)
+        GPIO.output(mode, int(self.value))
         self.mode = mode
 
     # sensor sensitivity settings
     def toggle_mode(self):
-        self.value = self.value + 1
-        GPIO.setup(self.mode, GPIO.OUT)
-        GPIO.output(self.mode, (self.value % 2))
+        self.value = (self.value + 1) % 2
+        GPIO.output(self.mode, int(self.value))
 
     def mode(self):
         return self.value
@@ -36,5 +51,6 @@ class WaterLevelSensor(Sensor):
 # How to use it
 TEST_MQTT_HOST = "test.mosquitto.org"
 TEST_MQTT_PORT = 1883
-sensor = WaterLevelSensor("water_level_sensor", SENSOR_CHANNEL, MODE_CHANNEL, TEST_MQTT_HOST, TEST_MQTT_PORT, "fish_tank/upper_water_level", 0, True, False)
+sensor = WaterLevelSensor("overflow_water_level_sensor", SENSOR_CHANNEL, MODE_CHANNEL, TEST_MQTT_HOST, TEST_MQTT_PORT, "fish_tank/water_level", 0, True, False)
+#sensor.toggle_mode()
 sensor.run()
--- src/water_valve.py
+++ src/water_valve.py
@@ -9,5 +9,5 @@ from actuator import Actuator
 WATER_VALVE_CHANNEL = 5
 TEST_MQTT_HOST = "test.mosquitto.org"
 TEST_MQTT_PORT = 1883
-water_valve = Actuator("water_valve", WATER_VALVE_CHANNEL, TEST_MQTT_HOST, TEST_MQTT_PORT, "fish_tank/upper_water_level", 0, False)
+water_valve = Actuator("water_valve", WATER_VALVE_CHANNEL, TEST_MQTT_HOST, TEST_MQTT_PORT, "fish_tank/water_level", 0, False)
 water_valve.run()
